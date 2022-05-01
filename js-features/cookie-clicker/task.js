2bf249eb16079ef987d48c7af639b00809fc6d9c
Anna Popova <panasenko-anna.8@mail.ru>
2022-05-01 15:47:51 +0300

    решает задачу

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,13 @@
+let clickCounter = document.getElementById('clicker__counter');
+
+let cookieImg = document.getElementById('cookie');
+
+cookieImg.onclick = function() {
+	if(cookieImg.width == 200) {
+		cookieImg.width = 300;
+	} else {
+		cookieImg.width = 200
+	}
+
+	clickCounter.textContent = +clickCounter.textContent + 1;
+}
