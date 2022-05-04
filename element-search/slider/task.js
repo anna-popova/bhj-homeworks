38b7b70326fc8ea7f91e71cee620e6ee65c4e35e
Anna Popova <panasenko-anna.8@mail.ru>
2022-05-04 21:53:08 +0300

    начинает решать задачу 3

---
+++ element-search/slider/task.js
@@ -0,0 +1,27 @@
+const sliderArrowPrev = document.querySelector('.slider__arrow_prev');
+const sliderArrowNext = document.querySelector('.slider__arrow_next');
+
+let step = 0;
+
+sliderArrowPrev.onclick = function() {
+	console.log(step);
+	step--;
+	if(step < 0) {
+		step = slider.length;
+	}
+}
+
+sliderArrowNext.onclick = function() {
+	console.log(step);
+	step++;
+	if(step == slider.length + 1) {
+		step = 0;
+	}
+}
+
+
+const slides = document.querySelectorAll('.slider__item');
+let slider = Array.from(slides);
+console.log(slider);
+
+console.log( slider.indexOf( document.querySelector('.slider__item_active') ) );
