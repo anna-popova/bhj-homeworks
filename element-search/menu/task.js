702f7cc9a37e5e793a066eceb8f5ad3b33ec343a
Anna Popova <panasenko-anna.8@mail.ru>
2022-05-04 20:54:05 +0300

    решает задачу 2

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -4,13 +4,12 @@ for (let i = 0; i <= menuLinks.length; i++) {
 	let menuLink = menuLinks[i];
 
 	let menuItem = menuLink.closest('.menu__item');
-	// console.log(menuItem);
-	let subMenu = menuItem.closest('.menu_sub');
+
+	const subMenu = menuItem.querySelector('.menu_sub');
 	console.log(subMenu);
 
 	menuLink.onclick = function() {
 		if(subMenu) {
-			// console.log('Hi');
 			subMenu.classList.toggle('menu_active');
 			return false;
 		}
