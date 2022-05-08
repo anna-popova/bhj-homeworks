cf3e99eca38f834dbaf541ed0cbd69536c123db4
Anna Popova <panasenko-anna.8@mail.ru>
2022-05-08 15:15:46 +0300

    исправляет замечания по задаче 2

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,6 +1,6 @@
 const menuLinks = document.querySelectorAll('.menu__link');
 
-for (let i = 0; i <= menuLinks.length; i++) {
+for (let i = 0; i < menuLinks.length; i++) {
 	let menuLink = menuLinks[i];
 
 	let menuItem = menuLink.closest('.menu__item');
