ea8eb8400a8e8063b429b1a7d6ced635d0de1c10
Anna Popova <panasenko-anna.8@mail.ru>
2022-05-10 12:37:45 +0300

    исправляет замечания по задаче 1

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -4,18 +4,12 @@ const showSuccessButton = document.querySelector('.show-success');
 const modalMainPopUp = document.getElementById('modal_main');
 const modalSuccessPopUp = document.getElementById('modal_success');
 
-const lastElCloseButtons = closeButtons.length - 1;
-
-// closeButton.onclick = function() {
-// 	modalMainPopUp.classList.remove('modal_active');
-// }
-
 showSuccessButton.onclick = function() {
 	modalMainPopUp.classList.remove('modal_active');
 	modalSuccessPopUp.classList.add('modal_active');
 }
 
-for (let i = 0; i < lastElCloseButtons; i++) {
+for (let i = 0; i < closeButtons.length; i++) {
 	closeButtons[i].onclick = function() {
 		//а внутри обработчика события с помощью контекста вызова (this) в котором находится объект 
 		//на котором произошло событие с помощью closest находить то модальное окно к которому принадлежит 
