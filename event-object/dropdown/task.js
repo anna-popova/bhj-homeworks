ae38f7bc6ced6d2950c3960aa310601f2ef1e0b2
Anna Popova <panasenko-anna.8@mail.ru>
2022-05-10 15:49:21 +0300

    продолжает  решать задачу 1

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,9 +1,30 @@
-const openButton = document.querySelector('.dropdown__value');
-// console.log(openButton);
+const dropdownValue = document.querySelector('.dropdown__value');
 const dropdownList = document.querySelector('.dropdown__list');
-// console.log(dropdownList);
+const dropdownItems = Array.from(document.querySelectorAll('.dropdown__item'));
 
-openButton.onсlick = function() {
-	console.log('HI');
+function toggleClassList() {
 	dropdownList.classList.toggle("dropdown__list_active");
+}
+
+function closeClassList() {
+	dropdownList.classList.remove("dropdown__list_active");
+}
+
+dropdownValue.addEventListener('click', toggleClassList);
+
+for(let i = 0; i < dropdownItems.length; i++) {
+	dropdownItem = dropdownItems[i];
+	const dropdownLink = dropdownItem.querySelector('.dropdown__link');
+
+	//насколько я понимаю, function changeValueName() не должна располагаться в цикле.
+	//но если вынести ее за пределы, как она получит доступ к dropdownLink за пределами цикла???
+	function changeValueName() {
+		dropdownList.classList.remove("dropdown__list_active");
+		dropdownValue.textContent = dropdownLink.textContent;
+		return false;
+	}
+
+	//на доли секунды текстовое содержание кнопки .dropdown__value меняется, но потом меняется обратно
+	//не совсем понимаю, почему так
+	dropdownItem.addEventListener('click', changeValueName);
 }
