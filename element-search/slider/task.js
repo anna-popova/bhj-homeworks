4c17b24826d69dcf5039092257e26c9cf97ba88b
Anna Popova <panasenko-anna.8@mail.ru>
2022-05-13 23:31:39 +0300

    вносит исправления в задачу 3

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -2,36 +2,16 @@ const sliderArrowPrev = document.querySelector('.slider__arrow_prev');
 const sliderArrowNext = document.querySelector('.slider__arrow_next');
 const sliders = Array.from(document.querySelectorAll('.slider__item'));
 
-let activeSlide = sliders.findIndex(item => item.name == item.querySelector('.slider__item_active'));
-console.log(activeSlide);
+let activeSlide = 0;
 
+function slide() {
 
+}
 
-// let step = 0;
-// sliderArrowPrev.onclick = function() {
-// 	if (step <= 0) {
-// 		step = lastIndex;
-// 	} else {
-// 		step--;
-// 	}
+sliderArrowPrev.addEventListener('click', () => {
 
-// 	setActiveImage(step);
-// }
+});
 
-// sliderArrowNext.onclick = function() {
-// 	if (step === lastIndex) {
-// 		step = 0;
-// 	} else {
-// 		step++;
-// 	}
+sliderArrowNext.addEventListener('click', () => {
 
-// 	setActiveImage(step);
-// }
-
-// setActiveImage = function(step) {
-// 	for (let i = 0; i < sliders.length; i++) {
-// 		const slide = sliders[i];
-// 		slide.classList.remove("slider__item_active");
-// 	}
-// 		sliders[step].classList.add("slider__item_active");
-// }
+});
