c924975922098c997a7a33342dcbc45a541b633b
Anna Popova <panasenko-anna.8@mail.ru>
2022-05-14 10:02:30 +0300

    решает задачу 3

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -2,16 +2,23 @@ const sliderArrowPrev = document.querySelector('.slider__arrow_prev');
 const sliderArrowNext = document.querySelector('.slider__arrow_next');
 const sliders = Array.from(document.querySelectorAll('.slider__item'));
 
-let activeSlide = 0;
+let activeSlide = sliders.findIndex((item) => item.classList.contains('slider__item_active'));
+// console.log(activeSlide);
 
-function slide() {
-
+function slide(newActiveSlide) {
+	sliders[activeSlide].classList.remove('slider__item_active');
+	sliders[newActiveSlide].classList.add('slider__item_active');
+	activeSlide = newActiveSlide;
 }
 
 sliderArrowPrev.addEventListener('click', () => {
-
+	let newSlide = activeSlide - 1;
+	newSlide = newSlide < 0 ? sliders.length - 1 : newSlide;
+	slide(newSlide);
 });
 
 sliderArrowNext.addEventListener('click', () => {
-
+	let newSlide = activeSlide + 1;
+	newSlide = newSlide >= sliders.length ? 0 : newSlide;
+	slide(newSlide);
 });
