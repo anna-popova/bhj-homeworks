acddf1e6e9f90c4f1b825496bf6683bd056e4e1d
Anna Popova <panasenko-anna.8@mail.ru>
2022-05-16 17:45:22 +0300

    начинает решать задачу 3

---
+++ dom/book-reader/task.js
@@ -0,0 +1,21 @@
+const bookControls = Array.from(document.querySelectorAll('.font-size'));
+const book = document.querySelector('.book');
+console.log(book);
+
+
+bookControls.forEach(elem => {
+	elem.addEventListener('click', (event) => {
+		event.preventDefault();
+
+		// elem.classList.toggle('font-size_active');
+
+		if (elem.classList.contains('font-size_small')) {
+			book.classList.add('book_fs-small');
+		} else if (elem.classList.contains('font-size_big')) {
+			book.classList.add('book_fs-big');
+		} else {
+			book.classList.remove('book_fs-small');
+			book.classList.remove('book_fs-big');
+		}
+	});
+})
