8c2e98d3c8d4711bf879c5922c8dba348cfff2fc
Anna Popova <panasenko-anna.8@mail.ru>
2022-05-19 20:52:41 +0300

    продолжает решать задачу Текстовый чат

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -1,8 +1,11 @@
 const openChatButton = document.querySelector('.chat-widget__side-text');
 const chatWidget = document.querySelector('.chat-widget');
-const messages = Array.from(document.querySelectorAll('.chat-widget__messages'));
-console.log(messages);
+const messages = document.querySelector('.chat-widget__messages')
 const chatWidgetInput = document.getElementById('chat-widget__input');
+
+const now = new Date();
+let hours = now.getHours();
+let minutes = now.getMinutes();
 
 //Реализуйте открытие окна чата при нажатии на красный боковой бэйдж
 openChatButton.addEventListener('click', () => {
@@ -11,24 +14,20 @@ openChatButton.addEventListener('click', () => {
 
 //По нажатию Enter, реализуйте отправку в чат непустого текстового сообщения
 document.addEventListener('keyup', (event) => {
-	//?проверка на непустое значение chatWidgetInput.textContent на стр. 13 верна?
-	// if (event.key == 'Enter' && chatWidgetInput.textContent !== '')
-	if (event.key == 'Enter') {
+
+	if (event.key === 'Enter' && chatWidgetInput.value !== '') {
+		const messageClientText = chatWidgetInput.value;
+
 		messages.innerHTML += `
 		<div class="message message_client">
-			//?как подставить текущее время?
-			<div class="message__time">09:21</div>
-			//?как вместо фиксированной строки вставить текст, который вводит пользователь?
-			<div class="message__text">
-				Добрый день, мы ещё не проснулись. Позвоните через 10 лет
-			</div>
+			<div class="message__time">${hours}:${minutes}</div>
+			<div class="message__text">${messageClientText}</div>
 		</div>
-		`;
-		console.log(messages);
+		`
+
+		chatWidgetInput.value = '';
 	}
 })
 
 //Реализуйте ответ робота (случайное сообщение из списка)
-//?Какого списка? Гле этот список взять?
-
-//Проставляйте реальную дату переписки
+//?Какого списка? Где этот список взять?
