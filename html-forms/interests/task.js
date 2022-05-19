638a37e015e59d17e27eb32f5cdef316db35d6c6
Anna Popova <panasenko-anna.8@mail.ru>
2022-05-19 21:30:22 +0300

    начинает решать задачу Дерево интересов

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,2 +1,22 @@
-const interests = Array.from(document.querySelectorAll('.interest__check'));
-console.log(interests);
+const interestCheckBoxes = Array.from(document.querySelectorAll('.interest__check'));
+// console.log(interestCheckBoxes);
+
+let highestIterestCheckBoxes = [];
+
+interestCheckBoxes.forEach(item => {
+	// console.log(item.closest('.interests_active'));
+
+	if (!item.closest('.interests_active')) {
+		highestIterestCheckBoxes.push(item);
+	}
+});
+
+console.log(highestIterestCheckBoxes);
+
+highestIterestCheckBoxes.forEach(item => {
+	item.addEventListener('change', () => {
+		console.log('Что делать дальше?');
+
+		//?не совсем понимаю, что делать дальше. Прошу подсказку.
+	})
+})
