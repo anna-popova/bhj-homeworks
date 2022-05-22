46eca815a937de048e84ae93012e92a4aaca067c
Anna Popova <panasenko-anna.8@mail.ru>
2022-05-22 13:33:24 +0300

    решает задачу 2 Ротатор рекламы

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,12 +1,19 @@
 const rotatorCases = Array.from(document.querySelectorAll('.rotator__case'));
-console.log(rotatorCases);
+// console.log(rotatorCases);
 
 function toggleClass() {
-	rotatorCases.forEach(elem => {
-		elem.classList.toggle('rotator__case_active');
-	})
-}
+	const rotatorCaseActive = document.querySelector('.rotator__case_active');
+	const nextRotatorCase = rotatorCaseActive.nextElementSibling;
 
-// toggleClass();
+	let activeCaseIndex = rotatorCases.findIndex((item) => item.classList.contains('rotator__case_active'));
+	// console.log(activeCaseIndex);
+
+	rotatorCaseActive.classList.remove('rotator__case_active');
+	if (activeCaseIndex === rotatorCases.length - 1) {
+		rotatorCases[0].classList.add('rotator__case_active');
+	} else {
+		nextRotatorCase.classList.add('rotator__case_active');
+	}
+}
 
 setInterval(toggleClass, 1000);
