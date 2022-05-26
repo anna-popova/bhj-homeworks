b5bb001e844b2edd7ccd68c68d9aed0d7b550910
Anna Popova <panasenko-anna.8@mail.ru>
2022-05-26 21:36:55 +0300

    исправляет замечания

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -6,19 +6,22 @@ const tasksList = document.getElementById('tasks__list');
 tasksAddButton.addEventListener('click', (e) => {
 	e.preventDefault();
 
+	taskInput.value = taskInput.value.trim();
+
 	if (taskInput.value !== '') {
-		tasksList.innerHTML += `
-			<div class="task">
-				<div class="task__title">${taskInput.value}</div>
-				<a href="#" class="task__remove">&times;</a>
-			</div>
-		`
+		tasksList.insertAdjacentHTML('afterend', `<div class="task"><div class="task__title">${taskInput.value}</div><a href="#" class="task__remove">&times;</a></div>`);
 	}
 
 	taskInput.value = '';
 
 	const taskRemoveButtons = Array.from(document.querySelectorAll('.task__remove'));
 
+	//Поэтому вам приходится перебирать все элементы крестиков и на каждый добавлять обработчик события.
+	//Если воспользуетесь методом insertAdjacentHTML, то разметка не будет полностью обновляться. 
+	//Будет добавляться новая разметка, а следовательно обработчики событий сохранятся и вам необходимо 
+	//будет добавлять слушатель события только на добавленный элемент.
+	//!ну мне в любом случае надо перебирать все крестики, иначе как я навешу событие на каждый из них
+	//!если их много, то в любои случае приходится их собирать в массив и проходиться по массиву
 	taskRemoveButtons.forEach(item => {
 		item.addEventListener('click', (e) => {
 			e.preventDefault();
@@ -27,4 +30,4 @@ tasksAddButton.addEventListener('click', (e) => {
 			itemParent.remove();
 		})
 	})
-})
+});
