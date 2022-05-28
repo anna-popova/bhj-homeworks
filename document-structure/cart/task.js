4c386ae16684f795bf352953fb4f1ebfa555dcdc
Anna Popova <panasenko-anna.8@mail.ru>
2022-05-28 13:43:17 +0300

    продолжает решать задачу

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -21,26 +21,25 @@ productQuantityControlsInc.forEach(item => {
 	})
 })
 
-const cart = {};
-
 productAddButton.forEach(item => {
 	item.addEventListener('click', () => {
 		const productItem = item.closest('.product');
 		const productImg = productItem.querySelector('img');
 		const productAmount = productItem.querySelector('.product__quantity-value');
 
-		cart[productItem.dataset.id] = {
-				count: productAmount,
-				html: `
-				<div class="cart__product" data-id="${productItem.dataset.id}">
-					<img class="cart__product-image" src="${productImg.src}">
-					<div class="cart__product-count">${productAmount.textContent}</div>
-				</div>
-				`,
-		}
-		cartProducts.innerHTML = '';
-		Object.keys(cart).forEach((id) => {
-			cartProducts.innerHTML += cart[id].html;
-		})
+		cartProducts.insertAdjacentHTML('beforeend', `<div class="cart__product" data-id="${productItem.dataset.id}"><img class="cart__product-image" src="${productImg.src}"><div class="cart__product-count">${+productAmount.textContent}</div>`);
+		const cartProductsCollection = Array.from(cartProducts.getElementsByClassName('cart__product'));
+		console.log(cartProductsCollection);
+
+		//использовать метод find для поиска нужного продукта из корзины
+		//?вот тут ступор. не очень понимаю, как именно мне find использовать.
+		//?скорее надо использовать if для проверки, если ли уже такой элемент в корзине?
+
+
+
+		// console.log(cartProductsCollection[0].dataset.id);
+		// console.log(productItem.dataset.id);
+		// console.log(cartProductsCollection[1].dataset.id);
+		// console.log(productItem.dataset.id);
 	})
 })
