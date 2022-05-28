226ef0b09c184325675266be4782f495920588da
Anna Popova <panasenko-anna.8@mail.ru>
2022-05-28 15:00:14 +0300

    реализует ответ бота

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -3,6 +3,9 @@ const chatWidget = document.querySelector('.chat-widget');
 const messages = document.querySelector('.chat-widget__messages')
 const chatWidgetInput = document.getElementById('chat-widget__input');
 
+const botGreeting = 'Приветики-пистолетики!';
+const botAnswers = ['Чё?', 'Да я хз вообще чё делать', 'Что ты хочешь от меня, я просто бот!', 'Я вообще не в теме', 'Ля-ля-ля', 'Не хочу! Не буду!'];
+
 const now = new Date();
 let hours = now.getHours();
 let minutes = now.getMinutes();
@@ -10,6 +13,14 @@ let minutes = now.getMinutes();
 //Реализуйте открытие окна чата при нажатии на красный боковой бэйдж
 openChatButton.addEventListener('click', () => {
 	chatWidget.classList.add('chat-widget_active');
+
+	//сразу сообщение от бота при открытии окна
+	messages.innerHTML += `
+	<div class="message">
+		<div class="message__time">${hours}:${minutes}</div>
+		<div class="message__text">${botGreeting}</div>
+	</div>
+	`
 });
 
 //По нажатию Enter, реализуйте отправку в чат непустого текстового сообщения
@@ -26,8 +37,21 @@ document.addEventListener('keyup', (event) => {
 		`
 
 		chatWidgetInput.value = '';
+
+		//сразу после сообщения пользователя приходит в чат сообщение от бота
+		getRandomBotAnswer();
 	}
 })
 
 //Реализуйте ответ робота (случайное сообщение из списка)
-//?Какого списка? Где этот список взять?
+function getRandomBotAnswer() {
+	const rand = Math.floor(Math.random()*botAnswers.length);
+	const rValue = botAnswers[rand];
+	
+	messages.innerHTML += `
+	<div class="message">
+		<div class="message__time">${hours}:${minutes}</div>
+		<div class="message__text">${rValue}</div>
+	</div>
+	`
+}
