17377e6b9c857356d9393fb27a077b2f432f396e
Anna Popova <panasenko-anna.8@mail.ru>
2022-05-29 19:40:20 +0300

    продолжает решать задачу cart

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -19,23 +19,24 @@ productQuantityControlsInc.forEach(item => {
 	item.addEventListener('click', () => {
 		+item.previousElementSibling.textContent++;
 	})
-})
+});
 
 productAddButton.forEach(item => {
 	item.addEventListener('click', () => {
 		const productItem = item.closest('.product');
 		const productImg = productItem.querySelector('img');
 		const productAmount = productItem.querySelector('.product__quantity-value');
-
-		cartProducts.insertAdjacentHTML('beforeend', `<div class="cart__product" data-id="${productItem.dataset.id}"><img class="cart__product-image" src="${productImg.src}"><div class="cart__product-count">${+productAmount.textContent}</div>`);
 		const cartProductsCollection = Array.from(cartProducts.getElementsByClassName('cart__product'));
+
+		//Если продукт не был найден, то добавляйте
+		//?почему не добавляется продукт?
+		if (cartProductsCollection.find(item => item.dataset.id !== productItem.dataset.id)) {
+			cartProducts.insertAdjacentHTML('beforeend', `<div class="cart__product" data-id="${productItem.dataset.id}"><img class="cart__product-image" src="${productImg.src}"><div class="cart__product-count">${+productAmount.textContent}</div>`);
+		} else {
+			//Если продукт был найден, то увеличивайте количество позиций в нём
+		}
+
 		console.log(cartProductsCollection);
-
-		//использовать метод find для поиска нужного продукта из корзины
-		//?вот тут ступор. не очень понимаю, как именно мне find использовать.
-		//?скорее надо использовать if для проверки, если ли уже такой элемент в корзине?
-
-
 
 		// console.log(cartProductsCollection[0].dataset.id);
 		// console.log(productItem.dataset.id);
