98187f7e28e94ea77e993ad1e221d4cc04e3d841
Anna Popova <panasenko-anna.8@mail.ru>
2022-05-30 19:41:11 +0300

    правит решение задачи 3 progressbar

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -3,22 +3,37 @@ const form = document.getElementById('form');
 
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
-	if (xhr.readyState == 1) {
-		progress.value = 0.25;
-	} else if (xhr.readyState == 2) {
-		progress.value = 0.5;
-	} else if (xhr.readyState == 3) {
-		progress.value = 0.75;
-	} else if (xhr.readyState == 4) {
-		progress.value = 1;
-	} else {
-		progress.value = 0;
+
+	const xhr = new XMLHttpRequest();
+	xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
+	const formData = new FormData(form);
+	xhr.send(formData);
+
+	xhr.upload.onloadstart = function() {
+		progress.value = 0.3;
 	}
+	xhr.upload.onprogress = function() {
+		progress.value = 0.7;
+	}
+	xhr.upload.onload = function() {
+		progress.value = 1;
+
+	// if (xhr.readyState == 1) {
+	// 	progress.value = 0.25;
+	// } else if (xhr.readyState == 2) {
+	// 	progress.value = 0.5;
+	// } else if (xhr.readyState == 3) {
+	// 	progress.value = 0.75;
+	// } else if (xhr.readyState == 4) {
+	// 	progress.value = 1;
+	// } else {
+	// 	progress.value = 0;
+	// }
 });
 
-const xhr = new XMLHttpRequest();
-xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
+// const xhr = new XMLHttpRequest();
+// xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
 
-const formData = new FormData(form);
+// const formData = new FormData(form);
 
-xhr.send(formData);
+// xhr.send(formData);
