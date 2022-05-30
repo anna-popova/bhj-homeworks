ccb0b13c981e0df39168d039302b8207902601b6
Anna Popova <panasenko-anna.8@mail.ru>
2022-05-30 19:18:01 +0300

    решает задачу 1 preloader

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -11,7 +11,7 @@ xhr.addEventListener('readystatechange', () => {
 		loader.classList.remove('loader_active');
 
 		const responseObj = xhr.response;
-		const responseObjValute = responseObj.response.Valute
+		const responseObjValute = responseObj.response.Valute;
 
 		for (let valute in responseObjValute) {
 			cardItem.insertAdjacentHTML('beforeend', `<div class="item__code">${responseObjValute[valute].CharCode}</div><div class="item__value">${responseObjValute[valute].Value}</div><div class="item__currency">руб.</div>`);
