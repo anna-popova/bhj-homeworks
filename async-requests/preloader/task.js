a1f06f7dc0dfb64b0506b9616f4db0b60696d02d
Anna Popova <panasenko-anna.8@mail.ru>
2022-05-30 19:17:26 +0300

    продолжает решать задачу cart

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -12,20 +12,10 @@ xhr.addEventListener('readystatechange', () => {
 
 		const responseObj = xhr.response;
 		const responseObjValute = responseObj.response.Valute
-		console.log(responseObjValute);
-		// console.log(Object.keys(responseObjValute));
-		// console.log(Object.values(responseObjValute));
 
-		// for(let valute in responseObjValute) {
-		// 	console.log(valute);
-		// }
-
-		//Все данные о курсе валют необходимо подгрузить в элемент #items
-		//В данном примере в .item__code нужно подгрузить значение свойства CharCode, в .item__value - Value
-
-		//?не понимаю, как мне добраться до значений свойст в CharCode и в Value
-
-		//cardItem.insertAdjacentHTML('beforeend', `<div class="item__code">${CharCode}</div><div class="item__value">${Value}</div><div class="item__currency">руб.</div>`);
+		for (let valute in responseObjValute) {
+			cardItem.insertAdjacentHTML('beforeend', `<div class="item__code">${responseObjValute[valute].CharCode}</div><div class="item__value">${responseObjValute[valute].Value}</div><div class="item__currency">руб.</div>`);
+		}
 	}
 });
 
