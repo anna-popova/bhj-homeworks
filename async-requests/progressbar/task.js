38c136aebed977dceab5ea09acad5598f7f3d75c
Anna Popova <panasenko-anna.8@mail.ru>
2022-05-31 19:43:25 +0300

    решает задачу 3 progressbar

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -5,31 +5,21 @@ form.addEventListener('submit', (e) => {
 	e.preventDefault();
 
 	const xhr = new XMLHttpRequest();
+
+	xhr.upload.onloadstart = function(event) {
+		console.log(event);
+		progress.value = 0.3;
+	}
+	xhr.upload.onprogress = function() {
+		console.log('2');
+		progress.value = 0.7;
+	}
+	xhr.upload.onload = function(event) {
+		console.log(event);
+		progress.value = 1;
+	}
+
 	xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
 	const formData = new FormData(form);
 	xhr.send(formData);
-
-	xhr.upload.onprogress = function(event) {
-		console.log(event);
-	}
-
-	// xhr.upload.onloadstart = function() {
-	// 	console.log('1')
-	// 	progress.value = 0.3;
-	// }
-	// xhr.upload.onprogress = function() {
-	// 	console.log('2')
-	// 	progress.value = 0.7;
-	// }
-	// xhr.upload.onload = function() {
-	// 	console.log('3')
-	// 	progress.value = 1;
-	// }
 });
-
-// const xhr = new XMLHttpRequest();
-// xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
-
-// const formData = new FormData(form);
-
-// xhr.send(formData);
