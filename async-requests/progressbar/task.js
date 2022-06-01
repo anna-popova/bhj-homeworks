702ed87d0c4e7d4aa740fdb4c545ae7ab8b0296b
Anna Popova <panasenko-anna.8@mail.ru>
2022-06-01 20:45:45 +0300

    вопрос по задаче progressbar

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -7,15 +7,15 @@ form.addEventListener('submit', (e) => {
 	const xhr = new XMLHttpRequest();
 
 	xhr.upload.onloadstart = function(event) {
-		console.log(event);
 		progress.value = 0.3;
 	}
-	xhr.upload.onprogress = function() {
-		console.log('2');
+	xhr.upload.onprogress = function(event) {
+		console.log(event);
+		//там есть два ключа, на основе которых вы сможете динамически заполнять шкалу прогресс бара.
+		//?честно говоря, вообще не понимаю, о каких именно ключах идет речь???
 		progress.value = 0.7;
 	}
 	xhr.upload.onload = function(event) {
-		console.log(event);
 		progress.value = 1;
 	}
 
