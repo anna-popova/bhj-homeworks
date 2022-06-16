0cd92e01509a2f7eee67c655162016eb9473fd99
Anna Popova <panasenko-anna.8@mail.ru>
2022-06-16 20:03:34 +0300

    исправляет замечания по задаче 1 - text-editor

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,27 +1,12 @@
 const editor = document.getElementById('editor');
 const editorText = {};
 
-editor.addEventListener('input', (e) => {
-	editorText.text = editor.value;
-	// console.log(editorText);
-	saveObj('text', editorText);
+editor.addEventListener('input', () => {
+	localStorage.setItem('key', editor.value);
 });
 
 window.onload = function() {
-	let newText = restoreObj('text');
-	// console.log(newText.text);
-	editor.value = newText.text;
-}
-
-function saveObj(key, obj) {
-	localStorage.setItem(key, JSON.stringify(obj));
-}
-
-function restoreObj(key) {
-	try {
-		return JSON.parse(localStorage.getItem(key));
-	} catch {
-		return null;
-	}
+	let newText = localStorage.getItem('key');
+	editor.value = newText;
 }
 
