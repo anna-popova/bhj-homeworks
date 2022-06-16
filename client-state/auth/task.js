3082624634a4ea40584bb34f628570bc74a89892
Anna Popova <panasenko-anna.8@mail.ru>
2022-06-16 20:44:28 +0300

    решает задачу 3 - auth

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,15 +1,30 @@
 const form = document.getElementById('signin__form');
+const userID = document.getElementById('user_id');
+const welcomeModal = document.getElementById('welcome');
 
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
 
 	const xhr = new XMLHttpRequest();
 	xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/auth.php');
+
+	xhr.responseType = 'json';
+
 	const formData = new FormData(form);
 	xhr.send(formData);
 
-	//вы получите JSON-ответ
-	//сохраните полученный id-пользователя в локальное хранилище
-	//? а как получить жтот самый JSON-ответ
-	console.log(xhr.response);
+	xhr.onload = function() {
+		let responseObj = xhr.response;
+		console.log(responseObj.user_id);
+
+		if(responseObj.user_id) {
+			userID.textContent = responseObj.user_id;
+			welcomeModal.classList.add('welcome_active');
+		} else {
+			alert('Неверный логин/пароль');
+		}
+	}
+
+	form.reset();
 });
+
