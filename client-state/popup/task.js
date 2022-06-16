dcea661f52e31f28279fefe7ddc80b7c50a6d92d
Anna Popova <panasenko-anna.8@mail.ru>
2022-06-16 20:18:43 +0300

    решает задачу 2 - popup

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,24 +1,21 @@
 const modalPopUp = document.getElementById('subscribe-modal');
 const closeButton = document.querySelector('.modal__close_times');
 
-	window.onload = function() {
-		modalPopUp.classList.add('modal_active');
-	}
-
 closeButton.addEventListener('click', (e) => {
 	modalPopUp.classList.remove('modal_active');
 	document.cookie = "closePopUp=true";
-	console.log(document.cookie);
+	getCookie('closePopUp');
 });
 
-// if (value) {
-// 	window.onload = function() {
-// 		modalPopUp.classList.add('modal_active');
-// 	}
-// }
+if (value) {
+	window.onload = function() {
+		modalPopUp.classList.add('modal_active');
+	}
+}
 
-// function getCookie(name) {
-// 	const pairs = document.cookie.split('; ');
-// 	const cookie = pairs.find(p => p.startsWith(name + '='));
-// 	return cookie.substr(name.length + 1);
-// }
+function getCookie(name) {
+	const pairs = document.cookie.split('; ');
+	const cookie = pairs.find(p => p.startsWith(name + '='));
+	value =  cookie.substr(name.length + 1);
+	return value;
+}
